e9d92c517599512722cafb53cc741891d49a532b
Yuuya SUGITA <yuuya.sugita@gmail.com>
2024-03-11 22:19:29 +0900

    ic_config: fix prebuild archive downloader

--- quel_ic_config/download_prebuilt.sh
+++ quel_ic_config/download_prebuilt.sh
@@ -3,8 +3,9 @@
 set -eu
 
 uribase=https://github.com/quel-inc/quelware/releases/download
-version="$(grep __version__ quel_ic_config/__init__.py | awk '{print $3}')"
+version="$(grep __version__ quel_ic_config/__init__.py | awk '{print $3}' | sed 's/\"//g')"
 archivename=quelware_prebuilt.tgz
 
+echo "INFO: downloading ${uribase}/${version}/${archivename} ..."
 wget -q "${uribase}/${version}/${archivename}" -O ${archivename} || (echo "ERROR: no prebuilt archive is available for ${version}" && exit 1)
 echo "INFO: download completed"
