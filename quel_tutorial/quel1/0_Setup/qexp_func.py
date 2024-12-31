70e59258292db2912e8f5bff0b1a347b28d0e0e5
Yuuya SUGITA <yuuya.sugita@gmail.com>
2024-12-31 22:02:23 +0900

    tutorial: Update tutorials for QuEL-1
    - revise explanations
    - refactoring python code
    - update and rename figures

--- quel_tutorial/quel1/0_Setup/qexp_func.py
+++ quel_tutorial/quel1/0_Setup/qexp_func.py
@@ -1,8 +1,5 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
-from typing import Union
-from IPython.display import display
 
 
 def square_pulse(
@@ -21,22 +18,3 @@ def square_pulse(
     wave[num_blank1 + num_flat : num_total] = 0.0 + 0.0j
 
     return wave * amplitude
-
-
-def plot_iq(
-    iq: npt.NDArray[np.complex64],
-    scale: Union[float, np.floating] = 1.0e4,
-    title: str = "wave data",
-):
-    plt.clf()
-    fig, ax = plt.subplots(1, 1, figsize=(12, 3))
-    ax.set_title(title)
-    ax.set_xlim(0, len(iq))
-    ax.set_ylim(-scale, scale)
-    ax.set_xlabel("Sample")
-    ax.set_ylabel("Amplitude")
-    ax.grid()
-    ax.plot(iq.real, label="I")
-    ax.plot(iq.imag, label="Q")
-    ax.legend()
-    plt.show()
