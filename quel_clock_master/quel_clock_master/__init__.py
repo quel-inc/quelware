0ca9f22046354144cc54f3bce1eac48a74ebee15
Yuuya SUGITA <yuuya.sugita@gmail.com>
2025-02-01 23:08:15 +0900

    clock_master: Remove
    - e7awghal replaces it.

--- quel_clock_master/quel_clock_master/__init__.py
+++
@@ -1,9 +0,0 @@
-from quel_clock_master.qubemasterclient import QuBEMasterClient
-from quel_clock_master.sequencerclient import SequencerClient
-
-__version__ = "0.1.6"
-
-__all__ = [
-    "QuBEMasterClient",
-    "SequencerClient",
-]
--- quel_clock_master/unittest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-if [ $# -ge 1 ] && [ "${1}" = "device" ]; then
-  PYTHONPATH=. pytest --log-cli-level=INFO --cov=quel_clock_master --cov-branch --cov-report=html --ignore=examples
-else
-  PYTHONPATH=. pytest --log-cli-level=INFO --cov=quel_clock_master --cov-branch --cov-report=html --ignore=examples --ignore=tests_with_devices
-fi
