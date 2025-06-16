120184e6cd49d9c44847d8aa1140206441819c6f
Ryosuke Imai <ryosukeimai89@gmail.com>
2025-06-16 17:43:11 +0900

    ic_config: setup uv

--- quel_ic_config/static_check.sh
+++ quel_ic_config/static_check.sh
@@ -2,18 +2,15 @@
 
 set -eu
 
-echo "[jq]"
-find src/quel_ic_config/settings -name "*.json" -print -exec jq "." {} \; > /dev/null
-find src/quel_ic_config_cli/settings -name "*.json" -print -exec jq "." {} \; > /dev/null
+echo "validate-json..."
+make validate-json
 
-echo "[clang-format]"
-clang-format -i adi_ad9082_v170/ad9082_wrapper.cpp
+echo "format check..."
+make format-cpp-check
+make format-py-check
 
-echo "[isort]"
-isort src tests testlibs scripts scripts_internal
-echo "[black]"
-black src tests testlibs scripts scripts_internal
-echo "[pflake8]"
-pflake8 src tests testlibs scripts scripts_internal
-echo "[mypy]"
-mypy --check-untyped-defs src/quel_ic_config src/quel_ic_config_utils src/quel_ic_config_cli tests scripts scripts_internal
+echo "lint..."
+make lint
+
+echo "typecheck..."
+make typecheck
