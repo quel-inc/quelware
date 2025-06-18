07c1b4473a663cf78bbcd3802b3e7d964bbc6040
Ryosuke Imai <ryosukeimai89@gmail.com>
2025-06-18 12:17:08 +0900

    e7awghal: setup uv

--- e7awghal/static_check.sh
+++ e7awghal/static_check.sh
@@ -2,11 +2,11 @@
 
 set -eu
 
-echo "[isort]"
-isort src tests scripts testlibs
-echo "[black]"
-black src tests scripts testlibs
-echo "[pflake8]"
-pflake8 src tests scripts testlibs
-echo "[mypy]"
-MYPYPATH=src:. mypy --check-untyped-defs --explicit-package-bases src/e7awghal tests scripts testlibs
+echo "format check..."
+make format-check
+
+echo "lint..."
+make lint
+
+echo "typecheck..."
+make typecheck
