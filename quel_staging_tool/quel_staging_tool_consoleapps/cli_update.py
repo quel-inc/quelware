a51097c897d41a0b282d31c367cb17c5b0e3347d
Ryosuke IMAI <ryosukeimai89@gmail.com>
2025-07-04 17:17:43 +0900

    staging_tool: Bump up to 0.2.10

--- quel_staging_tool/quel_staging_tool_consoleapps/cli_update.py
+++ quel_staging_tool/quel_staging_tool_consoleapps/cli_update.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from typing import Final
 
 from aiocoap import GET, PUT, Context, Message
+from packaging.version import Version
 
 from quel_staging_tool import QuelXilinxFpgaProgrammerZephyr
 from quel_staging_tool_consoleapps.common_parser import common_parser
@@ -20,65 +21,88 @@ EMPTY_HEXDIGEST: Final[str] = "7d44bb0955baeee64b2134d10e114fc0"
 
 
 class _ProgrammingTransportTuning:
-    ACK_TIMEOUT = 2.0
-    ACK_RANDOM_FACTOR = 1.5
     MAX_LATENCY = 180.0
-    DEFAULT_BLOCK_SIZE_EXP = 10
+    DEFAULT_BLOCK_SIZE_EXP = 11
+
+    ACK_TIMEOUT = 30.0
+    ACK_RANDOM_FACTOR = 1.5
     MAX_RETRANSMIT = 4
+    DEFAULT_LEISURE = 5
+    EMPTY_ACK_DELAY = 0.1
 
 
-async def update(ipaddr: IPv4Address, payload: bytes):
+async def update_and_verify(cmd_name: str, ipaddr: IPv4Address, payload: bytes, verify_len: int = 0):
     protocol = await Context.create_client_context()
-    request = Message(
+    request0 = Message(
+        code=GET,
+        uri=f"coap://{str(ipaddr)}/version/firmware",
+    )
+    request1 = Message(
+        code=GET,
+        uri=f"coap://{str(ipaddr)}/lock/acquire?t=180",
+    )
+    request2 = Message(
         code=PUT,
         uri=f"coap://{str(ipaddr)}/sys/firmware/update",
         payload=payload,
         transport_tuning=_ProgrammingTransportTuning,
     )
+    if verify_len != 0:
+        request3 = Message(code=GET, uri=f"coap://{str(ipaddr)}/sys/firmware/md5?s={verify_len}")
+    else:
+        request3 = Message(code=GET, uri=f"coap://{str(ipaddr)}/sys/firmware/md5")
+    request4 = Message(
+        code=GET,
+        uri=f"coap://{str(ipaddr)}/lock/release",
+    )
 
+    lock_acquired: bool = False
     try:
-        response = await protocol.request(request).response
+        response0 = await protocol.request(request0).response
+        if not response0.code.is_successful():
+            raise RuntimeError("failed to get the current firmware version")
+        logger.info(f"current firmware version: {response0.payload.decode()}")
+        need_to_lock: bool = cmd_name in {"quel_update_exstickge_1se", "quel_erase_exstickge_1se"} and Version(
+            response0.payload.decode()
+        ) >= Version("v1.3.0")
+        if need_to_lock:
+            response1 = await protocol.request(request1).response
+            if not response1.code.is_successful():
+                logger.error("failed to acquire lock, programming is aborted.")
+                return None
+            else:
+                lock_acquired = True
+                logger.info("lock is acquired")
+        response2 = await protocol.request(request2).response
+        if not response2.code.is_successful():
+            logger.error(f"programming failure with code {response2.code}")
+            return None
+        else:
+            logger.info("programming is done")
+        # Notes: 'verify' is executed in the locked environment in order to prevent the other client from halting.
+        #        be aware that the server cannot respond to any other requests during processing 'verify.'
+        response3 = await protocol.request(request3).response
+        if not response3.code.is_successful():
+            logger.error(f"hash calculation failure with code {response3.code}")
+            return None
+        else:
+            logger.info("hash calculation is done")
+
     except Exception as e:
         logger.error(f"failed to program the update region: {e}")
         return None
+    finally:
+        if lock_acquired:
+            response4 = await protocol.request(request4).response
+            if response4.code.is_successful():
+                logger.info("lock is released")
 
-    else:
-        return response
-
-
-async def verify(ipaddr: IPv4Address, size: int = 0):
-    protocol = await Context.create_client_context()
-    if size <= 0:
-        request = Message(code=GET, uri=f"coap://{str(ipaddr)}/sys/firmware/md5")
-    else:
-        request = Message(code=GET, uri=f"coap://{str(ipaddr)}/sys/firmware/md5?s={size}")
-
-    try:
-        response = await protocol.request(request).response
-    except Exception as e:
-        logger.error(f"failed to acquire md5 hash of the update region: {e}")
-        return None
-    else:
-        return response
+    return response3
 
 
-async def erase(ipaddr: IPv4Address):
-    protocol = await Context.create_client_context()
-    # Notes: I couldn't find a good way to set block_size to 1024 with null payload. this is just a workaround.
-    request = Message(
-        code=PUT,
-        uri=f"coap://{str(ipaddr)}/sys/firmware/update",
-        payload=bytes([0xFF]) * 2048,
-        transport_tuning=_ProgrammingTransportTuning,
-    )
-
-    try:
-        response = await protocol.request(request).response
-    except Exception as e:
-        logger.error(f"failed to erase the update region: {e}")
-        return None
-    else:
-        return response
+async def erase(cmd_name: str, ipaddr: IPv4Address):
+    payload = bytes([0xFF]) * 2048
+    return await update_and_verify(cmd_name, ipaddr, payload)
 
 
 def update_exstickge_clockdisty():
@@ -131,7 +155,7 @@ def update_exstickge_zephyr_common(cmd_name: str, bitfile_name: str):
         logger.error(e)
         sys.exit(1)
 
-    if update_body(obj, args.ipaddr, bitfile, mmifile, eelfpath):
+    if update_body(cmd_name, obj, args.ipaddr, bitfile, mmifile, eelfpath):
         logger.info("successful update, you need to reboot the target to run the updated firmware")
         sys.exit(0)
     else:
@@ -142,7 +166,12 @@ def update_exstickge_zephyr_common(cmd_name: str, bitfile_name: str):
 
 
 def update_body(
-    obj: QuelXilinxFpgaProgrammerZephyr, ipaddr: IPv4Address, bitfile: Path, mmifile: Path, eelfpath: Path
+    cmd_name: str,
+    obj: QuelXilinxFpgaProgrammerZephyr,
+    ipaddr: IPv4Address,
+    bitfile: Path,
+    mmifile: Path,
+    eelfpath: Path,
 ) -> bool:
     try:
         ebitpath: Path = obj.make_embedded_bit(bitpath=bitfile, mmipath=mmifile, elfpath=eelfpath)
@@ -150,18 +179,16 @@ def update_body(
         with open(ebinpath, "rb") as f:
             payload: bytes = f.read()
         hexdigest: str = hashlib.md5(payload).hexdigest()
-        res1 = asyncio.run(update(ipaddr, payload))
+        res1 = asyncio.run(update_and_verify(cmd_name, ipaddr, payload, len(payload)))
         if res1 is None:
+            # Notes: error message is already generated.
             return False
-        res2 = asyncio.run(verify(ipaddr, size=len(payload)))
-        if res2 is None:
-            return False
-        if res2.payload.decode() == hexdigest:
-            logger.info("the updated content is verified")
+        if res1.payload.decode() == hexdigest:
+            logger.info("the update is verified")
         else:
             logger.error(
                 "failed to verify the update content, "
-                f"md5 digest should be {hexdigest} but actually is {res2.payload.decode()}"
+                f"md5 digest should be {hexdigest} but actually is {res1.payload.decode()}"
             )
             return False
     except Exception as e:
@@ -199,28 +226,28 @@ def erase_exstickge_zephyr_common(cmd_name: str):
     if args.verbose:
         logging.getLogger().setLevel(logging.DEBUG)
 
-    if erase_body(args.ipaddr):
+    if erase_body(cmd_name, args.ipaddr):
         logger.info("successful removal of the updated firmware, fall-back firmware will run at the next reboot")
         sys.exit(0)
     else:
-        logger.error("failed clean-up of the update firmware although the update firmware is probably inactivated")
+        logger.error(
+            "failed clean-up of the update firmware. be aware that the update firmware may be disrupted partially"
+        )
         sys.exit(1)
 
 
-def erase_body(ipaddr: IPv4Address) -> bool:
+def erase_body(cmd_name: str, ipaddr: IPv4Address) -> bool:
     try:
-        res1 = asyncio.run(erase(ipaddr))
+        res1 = asyncio.run(erase(cmd_name, ipaddr))
         if res1 is None:
+            # Notes: error message is already generated.
             return False
-        res2 = asyncio.run(verify(ipaddr))
-        if res2 is None:
-            return False
-        if res2.payload.decode() == EMPTY_HEXDIGEST:
-            logger.info("the clean-up of the update region is verified")
+        if res1.payload.decode() == EMPTY_HEXDIGEST:
+            logger.info("the update region is confirmed to be blank")
         else:
             logger.error(
                 "failed to verify the clean-up of the update region, "
-                f"md5 digest should be {EMPTY_HEXDIGEST} but actually is {res2.payload.decode()}"
+                f"md5 digest should be {EMPTY_HEXDIGEST} but actually is {res1.payload.decode()}"
             )
             return False
     except Exception as e:
