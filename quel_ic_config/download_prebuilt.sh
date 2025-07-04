c839e8b1a6fde4b4ece3d5f415015829857e775e
Ryosuke IMAI <ryosukeimai89@gmail.com>
2025-07-04 21:47:37 +0900

    ic_config: Update the downloader script

--- quel_ic_config/download_prebuilt.sh
+++ quel_ic_config/download_prebuilt.sh
@@ -6,6 +6,18 @@ uribase=https://github.com/quel-inc/quelware/releases/download
 version="$(grep ^version pyproject.toml | awk '{print $3}' | sed 's/\"//g')"
 archivename=quelware_prebuilt.tgz
 
-echo "INFO: downloading ${uribase}/${version}/${archivename} ..."
-wget -q "${uribase}/${version}/${archivename}" -O ${archivename} || (echo "ERROR: no prebuilt archive is available for ${version}" && exit 1)
+tag_with_v="v${version}"
+tag_without_v="${version}"
+
+echo "INFO: downloading ${uribase}/${tag_with_v}/${archivename} ..."
+if ! wget -q "${uribase}/${tag_with_v}/${archivename}" -O "${archivename}"; then
+    echo "INFO: ${uribase}/${tag_with_v}/${archivename} not found."
+    echo "INFO: downloading ${uribase}/${tag_without_v}/${archivename} ..."
+    if ! wget -q "${uribase}/${tag_without_v}/${archivename}" -O "${archivename}"; then
+        echo "INFO: ${uribase}/${tag_without_v}/${archivename} not found."
+        echo "ERROR: no prebuilt archive is available for version ${version}"
+        exit 1
+    fi
+fi
+
 echo "INFO: download completed"
