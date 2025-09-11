77fd343b1b866d0073236aa82f53932a5fb59f83
Ryosuke IMAI <ryosuke@imaimaimai.net>
2025-09-12 00:18:19 +0900

    ic_config: update conftests to separate environment-specific info.

---
+++ quel_ic_config/tests/conftest.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+# Add the quel_ic_config dir to search path to find `testlib`.
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
--- quel_ic_config/tests/with_devices/conftest.py
+++ quel_ic_config/tests/with_devices/conftest.py
@@ -1,20 +1,22 @@
 import logging
 import os
 import shutil
+from collections.abc import Iterator
 from pathlib import Path
-from typing import Any, Generator
+from typing import Generator, Union
 
 import pytest
+import yaml
 from quel_inst_tool import SpectrumAnalyzer
 
 from quel_ic_config.quel1_box import Quel1Box, Quel1BoxType
+from quel_ic_config_utils import configuration
 from testlibs.register_cw import register_cw_to_all_ports
 from testlibs.spa_helper import init_e440xb, init_ms2xxxx, measure_floor_noise
 
 logger = logging.getLogger(__name__)
 
-if (artifacts_path := os.getenv("QUEL_TESTING_ARTIFACTS_DIR")) is None:
-    artifacts_path = "./artifacts"
+artifacts_path = str(os.getenv("QUEL_TESTING_ARTIFACTS_DIR", "./artifacts"))
 
 TEST_SETTINGS_MS2720T1 = (
     {
@@ -26,73 +28,6 @@ TEST_SETTINGS_MS2720T1 = (
             "resolution_bandwidth": 1e4,
         },
         "max_background_noise": -50.0,
-    },
-)
-
-TEST_SETTINGS_QUEL1 = (
-    {
-        "label": "staging-074",
-        "box_config": {
-            "ipaddr_wss": "10.1.0.74",
-            "ipaddr_sss": "10.2.0.74",
-            "ipaddr_css": "10.5.0.74",
-            "boxtype": Quel1BoxType.fromstr("quel1-a"),
-        },
-        "linkup_config": {
-            "mxfes_to_linkup": (0, 1),
-            "use_204b": False,
-        },
-        "port_availability": {
-            "unavailable": [],
-            "via_monitor_out": [],
-        },
-        "image_path": artifacts_path,
-        "relative_loss": 0,
-        "linkup": False,
-    },
-    {
-        "label": "staging-050",
-        "box_config": {
-            "ipaddr_wss": "10.1.0.50",
-            "ipaddr_sss": "10.2.0.50",
-            "ipaddr_css": "10.5.0.50",
-            "boxtype": Quel1BoxType.fromstr("quel1-a"),
-        },
-        "linkup_config": {
-            "mxfes_to_linkup": (0, 1),
-            "use_204b": False,
-        },
-        "port_availability": {
-            "unavailable": [],
-            "via_monitor_out": [],
-        },
-        "image_path": artifacts_path,
-        "relative_loss": 0,
-        "linkup": False,
-    },
-    {
-        "label": "staging-060",
-        "box_config": {
-            "ipaddr_wss": "10.1.0.60",
-            "ipaddr_sss": "10.2.0.60",
-            "ipaddr_css": "10.5.0.60",
-            "boxtype": Quel1BoxType.fromstr("quel1-b"),
-        },
-        "linkup_config": {
-            "mxfes_to_linkup": (0, 1),
-            "ignore_crc_error_of_mxfe": (0, 1),
-            "use_204b": False,
-        },
-        "reconnect_config": {
-            "ignore_crc_error_of_mxfe": (0, 1),
-        },
-        "port_availability": {
-            "unavailable": [],
-            "via_monitor_out": [],
-        },
-        "image_path": artifacts_path,
-        "relative_loss": 9,
-        "linkup": False,
     },
 )
 
@@ -110,81 +45,8 @@ TEST_SETTINGS_E4405B = (
     },
 )
 
-TEST_SETTINGS_RIKEN8 = (
-    {
-        "label": "staging-094",
-        "box_config": {
-            "ipaddr_wss": "10.1.0.94",
-            "ipaddr_sss": "10.2.0.94",
-            "ipaddr_css": "10.5.0.94",
-            "boxtype": Quel1BoxType.fromstr("quel1se-riken8"),
-        },
-        "linkup_config": {
-            "mxfes_to_linkup": (0, 1),
-            "use_204b": False,
-        },
-        "port_availability": {
-            "unavailable": [],
-            "via_monitor_out": [],
-        },
-        "image_path": artifacts_path,
-        "relative_loss": 0,
-        "linkup": False,
-    },
-)
-
-TEST_SETTINGS_FUJITSU11A = (
-    {
-        "label": "staging-157",
-        "box_config": {
-            "ipaddr_wss": "10.1.0.157",
-            "ipaddr_sss": "10.2.0.157",
-            "ipaddr_css": "10.5.0.157",
-            "boxtype": Quel1BoxType.fromstr("quel1se-fujitsu11-a"),
-        },
-        "linkup_config": {
-            "mxfes_to_linkup": (0, 1),
-            "use_204b": False,
-        },
-        "port_availability": {
-            "unavailable": [],
-            "via_monitor_out": [],
-        },
-        "spa_type": "",
-        "image_path": artifacts_path,
-        "relative_loss": 0,
-        "linkup": False,
-    },
-)
-
-
-TEST_SETTINGS_FUJITSU11B = (
-    {
-        "label": "staging-164",
-        "box_config": {
-            "ipaddr_wss": "10.1.0.164",
-            "ipaddr_sss": "10.2.0.164",
-            "ipaddr_css": "10.5.0.164",
-            "boxtype": Quel1BoxType.fromstr("quel1se-fujitsu11-b"),
-        },
-        "linkup_config": {
-            "mxfes_to_linkup": (0, 1),
-            "use_204b": False,
-        },
-        "port_availability": {
-            "unavailable": [],
-            "via_monitor_out": [],
-        },
-        "spa_type": "",
-        "image_path": artifacts_path,
-        "relative_loss": 0,
-        "linkup": False,
-    },
-)
-
 
 def make_topoutdir(param) -> Path:
-
     dirpath = Path(param["image_path"]) / param["label"]
     if os.path.exists(dirpath):
         logger.info(f"deleting the existing directory: '{dirpath}'")
@@ -192,23 +54,9 @@ def make_topoutdir(param) -> Path:
     return dirpath
 
 
-def make_box_fixture(param0):
-    topdirpath = make_topoutdir(param0)
-
-    box = Quel1Box.create(**param0["box_config"])
-    assert box.has_lock, "no lock is available"
-
-    if param0["linkup"]:
-        linkstatus = box.relinkup(**param0["linkup_config"])
-    else:
-        cfg = param0["reconnect_config"] if "reconnect_config" in param0 else {}
-        linkstatus = box.reconnect(**cfg)
-    assert linkstatus[0]
-    assert linkstatus[1]
-
-    register_cw_to_all_ports(box)
-
-    return box, topdirpath
+def prepare_artifact_dir(label: str) -> Path:
+    dirpath = Path(artifacts_path) / label
+    return dirpath
 
 
 def make_spa_fixture(param0):
@@ -237,22 +85,6 @@ def fixture_ms2720t1(
     del spa
 
 
-@pytest.fixture(scope="session", params=TEST_SETTINGS_QUEL1)
-def fixtures1(
-    request,
-) -> Generator[tuple[Quel1Box, dict[str, Any], Path], None, None]:
-    param0 = request.param
-
-    box, topdirpath = make_box_fixture(param0)
-
-    yield box, param0, topdirpath
-
-    box.initialize_all_awgunits()
-    box.activate_monitor_loop(0)
-    box.activate_monitor_loop(1)
-    del box
-
-
 @pytest.fixture(scope="session", params=TEST_SETTINGS_E4405B)
 def fixture_e4405b(
     request,
@@ -263,49 +95,77 @@ def fixture_e4405b(
     del spa
 
 
-@pytest.fixture(scope="session", params=TEST_SETTINGS_RIKEN8)
-def fixtures8(
-    request,
-) -> Generator[tuple[Quel1Box, dict[str, Any], Path], None, None]:
-    param0 = request.param
-
-    box, topdirpath = make_box_fixture(param0)
-
-    yield box, param0, topdirpath
-
-    box.initialize_all_awgunits()
-    box.activate_monitor_loop(0)
-    box.activate_monitor_loop(1)
-    del box
+def pytest_addoption(parser):
+    parser.addoption("--sysconf", action="store", help="path to sysconf file (.yaml)")
 
 
-@pytest.fixture(scope="session", params=TEST_SETTINGS_FUJITSU11A)
-def fixtures11a(
-    request,
-) -> Generator[tuple[Quel1Box, dict[str, Any], Path], None, None]:
-    param0 = request.param
+@pytest.fixture(scope="session")
+def sysconf(request):
+    sysconf_path = request.config.getoption("--sysconf")
+    if sysconf_path:
+        if not os.path.exists(sysconf_path):
+            raise FileNotFoundError(f"system configuration file is not found: '{sysconf_path}'")
+        with open(sysconf_path) as io:
+            obj = yaml.safe_load(io)
+            sysconf = configuration.SystemConfiguration.model_validate(obj)
+    else:
+        logger.info("Default system configuration will be used.")
+        sysconf = configuration.load_default_configuration()
+    return sysconf
 
-    box, topdirpath = make_box_fixture(param0)
 
-    yield box, param0, topdirpath
+class BoxProvider:
+    def __init__(self, sysconf: configuration.SystemConfiguration):
+        self._name_to_existing_box: dict[str, Quel1Box] = {}
+        self._sysconf = sysconf
 
-    box.initialize_all_awgunits()
-    box.activate_monitor_loop(0)
-    box.activate_monitor_loop(1)
-    del box
+    def _create_and_reconnect(self, boxconf: configuration.Box):
+        box = next(configuration.get_boxes((boxconf,)))
+        assert box.has_lock, "no lock is available"
+
+        status = configuration.reconnect_and_get_link_status(
+            box, ignore_crc_error_of_mxfe=boxconf.ignore_crc_error_of_mxfe
+        )
+        if not all(status.values()):
+            raise ValueError(f"Wrong link status on {box.name}: {status}")
+        register_cw_to_all_ports(box)
+        return box
+
+    def get_box_from_type(self, boxtype: Union[Quel1BoxType, str], skip_if_not_found: bool = True) -> Quel1Box:
+        boxconf = next(self.find_boxconf_from_type(boxtype, skip_if_not_found))
+        if boxconf.name not in self._name_to_existing_box:
+            box = self._create_and_reconnect(boxconf)
+            self._name_to_existing_box[boxconf.name] = box
+        return self._name_to_existing_box[boxconf.name]
+
+    def find_boxconf_from_type(
+        self, boxtype: Union[Quel1BoxType, str], skip_if_not_found: bool = True
+    ) -> Iterator[configuration.Box]:
+        if isinstance(boxtype, str):
+            boxtype = Quel1BoxType.fromstr(boxtype)
+        for b in self._sysconf.boxes:
+            if Quel1BoxType.fromstr(b.boxtype) is boxtype:
+                yield b
+        if skip_if_not_found:
+            pytest.skip(reason=f"box with type `{boxtype}` not found in system configuration.")
+        raise ValueError(
+            f"box with type `{boxtype}` not found in system configuration. Ensure the '--sysconf' option is set correctly."
+        )
+
+    def clean_up(self):
+        for box in self._name_to_existing_box.values():
+            box.initialize_all_awgunits()
+            if box.boxtype != "quel1-nec":
+                box.activate_monitor_loop(0)
+                box.activate_monitor_loop(1)
+        for name in [k for k in self._name_to_existing_box.keys()]:
+            del self._name_to_existing_box[name]
 
 
-@pytest.fixture(scope="session", params=TEST_SETTINGS_FUJITSU11B)
-def fixtures11b(
-    request,
-) -> Generator[tuple[Quel1Box, dict[str, Any], Path], None, None]:
-    param0 = request.param
-
-    box, topdirpath = make_box_fixture(param0)
-
-    yield box, param0, topdirpath
-
-    box.initialize_all_awgunits()
-    box.activate_monitor_loop(0)
-    box.activate_monitor_loop(1)
-    del box
+@pytest.fixture(scope="session")
+def box_provider(
+    sysconf,
+) -> Generator[BoxProvider, None, None]:
+    box_repo = BoxProvider(sysconf)
+    yield box_repo
+    box_repo.clean_up()
